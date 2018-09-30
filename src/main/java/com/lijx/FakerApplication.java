88871de613e49911b8ffb06521200f00d99da13b
lijx <lijx@mo9.com>
2018-09-30 14:33:44 +0800

    Initial commit

---
+++ src/main/java/com/lijx/FakerApplication.java
@@ -0,0 +1,12 @@
+package com.lijx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakerApplication.class, args);
+	}
+}
